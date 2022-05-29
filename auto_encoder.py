ac661f03c89db306f0816c7e23451f5d4ee1b965
srpkdyy <srpkdyy@gmail.com>
2022-05-29 15:53:31 +0900

    implemented

---
+++ auto_encoder.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+
+
+def make_encode_layers(n_in, n_out):
+    layers = [
+        nn.Conv2d(n_in, n_out, kernel_size=3, padding=1),
+        nn.BatchNorm2d(n_out),
+        nn.ReLU(inplace=True),
+        nn.MaxPool2d(2, 2)
+    ]
+    return nn.Sequential(*layers)
+
+
+def make_decode_layers(n_in, n_out):
+    layers = [
+        nn.ConvTranspose2d(n_in, n_out, kernel_size=2, stride=2),
+        nn.BatchNorm2d(n_out),
+        nn.ReLU(inplace=True)
+    ]
+    return nn.Sequential(*layers)
+
+    
+class AutoEncoder(nn.Module):
+    def __init__(self, n_channels=3, dims=(16, 32, 64, 128)):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            make_encode_layers(n_classes, dims[0]),
+            make_encode_layers(dims[0], dims[1]),
+            make_encode_layers(dims[1], dims[2]),
+            make_encode_layers(dims[2], dims[3])
+        )
+        self.decoder = nn.Sequential(
+            make_decode_layers(dims[3], dims[2]),
+            make_decode_layers(dims[2], dims[1]),
+            make_decode_layers(dims[1], dims[0]),
+            make_decode_layers(dims[0], n_channels)
+        )
+
+    def forward(self, x):
+        out = self.encoder(x)
+        out = self.decoder(out)
+        return out
+    
+    def encode(self, x):
+        out = self.encoder(x)
+        out = torch.flatten(out, 1)
+        return out
+
---
+++ main.py
@@ -0,0 +1,60 @@
+import argparse
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from tqdm import tqdm
+from torchvision import datasets
+from torchvision import transforms as T
+from .auto_encoder import AutoEncoder
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--in-dir', type=str, default='./data')
+parser.add_argument('-d', '--out-dir', type=str, default='./output')
+parser.add_argument('-b', '--batch-size'type=int, default=128)
+parser.add_argument('-e', '--epochs', type=int, default=30)
+args = parser.parse_args()
+
+
+use_cuda = torch.cuda.is_available()
+device = torch.device('cuda' if use_cuda else 'cpu')
+dcount = max(1, torch.cuda.device_count())
+
+args.batch_size *= dcount
+kwargs = {
+    'num_workers': os.cpu_count(),
+    'pin_memory': True
+    } if use_cuda else {}
+
+dataloader = torch.utils.data.DataLoader(
+    datasets.CIFAR10(args.in_dir, train=True, download=True, transform=T.ToTensor()),
+    batch_size=args.batch_size, shuffle=True, **kwargs)
+
+model = AutoEncoder().to(device)
+if dcount > 1:
+    model = nn.DataParallel(model)
+    torch.backends.cudnn.benchmark = True
+model.train()
+
+optimizer = optim.Adam(model.parameters(), lr=1e-3*dcount)
+criterion = nn.BCELoss()
+
+for epoch in args.epochs:
+    total_loss = 0
+    for img, _ in tqdm(dataloader):
+        img = img.to(device)
+
+        recon = model(img)
+        loss = criterion(recon, img)
+
+        optimizer.zero_grad()
+        loss.backward()
+        total_loss += loss.item()
+        optimizer.step()
+
+    print(f'Epoch: {epoch} Loss: {total_loss:.4f}')
+
+torch.save(model.state_dict(), f'{args.out_dir}/latest.pth')
+print('Save model weights')
+
