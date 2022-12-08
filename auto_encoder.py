f888e03f49793348e09ad2dcaaa1892b95a77db1
srpkdyy <srpkdyy@gmail.com>
2022-12-09 01:02:15 +0900

    upgrade implementation of auto-encoder

--- auto_encoder.py
+++ auto_encoder.py
@@ -2,50 +2,65 @@ import torch
 import torch.nn as nn
 
 
-def make_encode_layers(n_in, n_out):
-    layers = [
-        nn.Conv2d(n_in, n_out, kernel_size=3, padding=1),
-        nn.BatchNorm2d(n_out),
-        nn.ReLU(inplace=True),
-        nn.MaxPool2d(2, 2)
-    ]
-    return nn.Sequential(*layers)
-
-
-def make_decode_layers(n_in, n_out):
-    layers = [
-        nn.ConvTranspose2d(n_in, n_out, kernel_size=2, stride=2),
-        nn.BatchNorm2d(n_out),
-        nn.ReLU(inplace=True)
-    ]
-    return nn.Sequential(*layers)
-
-    
-class AutoEncoder(nn.Module):
-    def __init__(self, n_channels=3, dims=(16, 32, 64, 128)):
+class ConvBlock(nn.Module):
+    def __init__(self, ch, out_ch=None, k=3, s=1, p=1, groups=32):
         super().__init__()
-        self.encoder = nn.Sequential(
-            make_encode_layers(n_channels, dims[0]),
-            make_encode_layers(dims[0], dims[1]),
-            make_encode_layers(dims[1], dims[2]),
-            make_encode_layers(dims[2], dims[3])
-        )
-        self.decoder = nn.Sequential(
-            make_decode_layers(dims[3], dims[2]),
-            make_decode_layers(dims[2], dims[1]),
-            make_decode_layers(dims[1], dims[0]),
-            make_decode_layers(dims[0], n_channels)
-        )
-        self.sigmoid = nn.Sigmoid()
+        out_ch = out_ch or ch
+        self.act = nn.SiLU()
+        self.norm = nn.GroupNorm(groups, ch)
+        self.conv = nn.Conv2d(ch, out_ch, k, s, p, padding_mode='replicate')
 
     def forward(self, x):
-        out = self.encoder(x)
-        out = self.decoder(out)
-        out = self.sigmoid(out)
-        return out
-    
-    def encode(self, x):
-        out = self.encoder(x)
-        out = torch.flatten(out, 1)
-        return out
+        return self.conv(self.norm(self.act(x)))
+
+
+class ConvTransposeBlock(ConvBlock):
+    def __init__(self, ch, out_ch=None, k=3, s=1, p=1, groups=32):
+        super().__init__(ch, out_ch, k, s, p, groups)
+        self.conv = nn.ConvTranspose2d(ch, out_ch, k, s, p)
+
+
+class ResBlock(nn.Module):
+    def __init__(self, ch):
+        super().__init__()
+        self.block1 = ConvBlock(ch)
+        self.block2 = ConvBlock(ch)
+
+    def forward(self, x):
+        return x + self.block2(self.block1(x))
+
+
+class AutoEncoder(nn.Module):
+    def __init__(self, ch=3, dim=128, mults=(1, 2, 4), z_dim=4):
+        super().__init__()
+        self.dim = dim
+
+        encoder = [nn.Conv2d(ch, dim, 3, 1, 1, padding_mode='replicate')]
+        for m in mults:
+            encoder.append(ConvBlock(dim, self.dim * m, k=4, s=2))
+            dim = self.dim * m
+            encoder.append(ResBlock(dim))
+            encoder.append(ResBlock(dim))
+        encoder.append(nn.Conv2d(dim, z_dim, 1))
+
+        decoder = [nn.Conv2d(z_dim, dim, 1)]
+        for m in reversed(mults):
+            decoder.append(ConvTransposeBlock(dim, self.dim * m, k=4, s=2, p=1))
+            dim = self.dim * m
+            decoder.append(ResBlock(dim))
+            decoder.append(ResBlock(dim))
+        decoder.append(nn.Conv2d(dim, ch, 3, 1, 1, padding_mode='replicate'))
+
+        self.encoder = nn.Sequential(*encoder)
+        self.decoder = nn.Sequential(*decoder)
+
+    def forward(self, x):
+        return self.decoder(self.encoder(x))
+
+
+if __name__ == '__main__':
+    cls = AutoEncoder()
+    img = torch.rand(1, 3, 96, 96)
+    print(cls)
+    print(cls(img).shape)
 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import torch
+import torchvision
 import torch.nn as nn
 import torch.optim as optim
 from tqdm import tqdm
@@ -17,6 +18,7 @@ parser.add_argument('-e', '--epochs', type=int, default=100)
 parser.add_argument('-lr', type=float, default=1e-4)
 args = parser.parse_args()
 
+os.makedirs(args.out_dir, exist_ok=True)
 
 use_cuda = torch.cuda.is_available()
 device = torch.device('cuda' if use_cuda else 'cpu')
@@ -28,8 +30,12 @@ kwargs = {
     'pin_memory': True
     } if use_cuda else {}
 
+transform = T.Compose([
+    T.ToTensor(),
+    T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
 dataloader = torch.utils.data.DataLoader(
-    datasets.CIFAR10(args.in_dir, train=True, download=True, transform=T.ToTensor()),
+    datasets.STL10(args.in_dir, split='train', download=True, transform=transform),
     batch_size=args.batch_size, shuffle=True, **kwargs)
 
 model = AutoEncoder().to(device)
@@ -39,7 +45,7 @@ if dcount > 1:
 model.train()
 
 optimizer = optim.Adam(model.parameters(), lr=args.lr*dcount)
-criterion = nn.BCELoss(reduction='sum')
+criterion = nn.L1Loss().to(device)
 
 for epoch in range(args.epochs):
     total_loss = 0
@@ -55,8 +61,8 @@ for epoch in range(args.epochs):
         optimizer.step()
 
     print(f'Epoch: {epoch} Loss: {total_loss/len(dataloader.dataset):.4f}')
+    torchvision.utils.save_image(torch.cat((img[:8], recon[:8])), f'{args.out_dir}/{epoch}.png')
 
-os.makedirs(args.out_dir, exist_ok=True)
 torch.save(model.state_dict(), f'{args.out_dir}/latest.pth')
 print('Save model weights')
 
